c0d2cee1d5ebba6025beeb8bc29c9d56abe2832a
Carlinhos <carloscenachi@protonmail.com>
2021-09-08 18:13:47 -0300

    second

--- Exer07.java
+++ Exer07.java
@@ -2,45 +2,42 @@
 import java.util.Scanner;
 
 class Main {
-    
-    public static float somaLinha(float matriz[][], int linha){
+
+    public static float somaLinha(float matriz[][], int linha) {
+
+        float soma = 0;
         
-        float soma=0;
-        
-        for (int i = 0; i <3; i++) {
-            for (int j = 0; j <2; j++) {
-                if(linha == i){
-                    soma += matriz[linha][j];
-                }
-            }
-        }        
+        for (int j = 0; j < 2; j++) {
+            soma += matriz[linha][j];
+        }
+
         return soma;
     }
-    
+
     public static void main(String[] args) {
-        
+
         Scanner teclado = new Scanner(System.in);
-        
+
         float matriz[][] = new float[3][2];
-        
-        for (int i = 0; i <3; i++) {
-            for (int j = 0; j <2; j++) {
-                matriz[i][j] = teclado.nextFloat();                
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matriz[i][j] = teclado.nextFloat();
             }
         }
-        
+
         int linha = teclado.nextInt();
-        
+
         teclado.close();
-        
+
         System.out.println("Matriz:");
-        for (int i = 0; i <3; i++) {
-            for (int j = 0; j <2; j++) {
-                System.out.print(matriz[i][j]+" ");              
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matriz[i][j] + " ");
             }
             System.out.println("");
         }
-        
-        System.out.println("\nSoma dos elementos da linha "+ linha +":\n"+somaLinha(matriz, linha));
+
+        System.out.println("\nSoma dos elementos da linha " + linha + ":\n" + somaLinha(matriz, linha));
     }
 }
